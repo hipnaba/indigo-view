54019f7d991573596b6dcecbce40b6188a70254e
hipnaba <hipnaba@gmail.com>
2017-02-27 16:40:54 +0100

    added a PhpRenderer extension

---
+++ src/Renderer/PhpRenderer.php
@@ -0,0 +1,16 @@
+<?php
+namespace Indigo\View\Renderer;
+
+use Zend\View\Renderer\PhpRenderer as BasePhpRenderer;
+
+/**
+ * An extension to Zend's PhpRenderer.
+ *
+ * @package Indigo\View\Renderer
+ * @author  Danijel Fabijan <hipnaba@gmail.com>
+ * @link    https://github.com/hipnaba/indigo-view
+ */
+class PhpRenderer extends BasePhpRenderer
+{
+
+}
