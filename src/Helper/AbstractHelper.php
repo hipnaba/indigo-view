e36f5138f378d4b537ee0a6900fdcf30e6ebed61
hipnaba <hipnaba@gmail.com>
2017-02-27 04:44:59 +0100

    added a utility function for fetching helper plugins

--- src/Helper/AbstractHelper.php
+++ src/Helper/AbstractHelper.php
@@ -2,7 +2,7 @@
 namespace Indigo\View\Helper;
 
 use Zend\View\Helper\AbstractHelper as BaseAbstractHelper;
-use Zend\View\Renderer\PhpRenderer;
+use Zend\View\Renderer\RendererInterface;
 
 /**
  * Base Helper plugin class.
@@ -10,10 +10,31 @@ use Zend\View\Renderer\PhpRenderer;
  * @package Indigo\View\Helper
  * @author  Danijel Fabijan <hipnaba@gmail.com>
  * @link    https://github.com/hipnaba/indigo-view
- *
- * @method PhpRenderer getView()
  */
 abstract class AbstractHelper extends BaseAbstractHelper
 {
+    /**
+     * Fetches a helper plugin from the renderer.
+     *
+     * @param string $plugin The plugin's name.
+     *
+     * @return callable
+     */
+    protected function getHelperPlugin($plugin)
+    {
+        $renderer = $this->getView();
 
+        if (!$renderer instanceof RendererInterface) {
+            throw new \RuntimeException('Renderer not set.');
+        }
+
+        if (!method_exists($renderer, 'plugin')) {
+            throw new \RuntimeException(sprintf(
+                "The renderer '%s' isn't pluggable.",
+                get_class($renderer)
+            ));
+        }
+
+        return $renderer->plugin($plugin);
+    }
 }
